376e07e7952f966b06d247d86e8fd033aa0f0121
sbvinay05 <133862038+sbvinay05@users.noreply.github.com>
2023-07-19 02:45:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.post('/api', (req, res) => {
+  const json_data = req.body.json_data;
+  const parsed_data = JSON.parse(json_data) || {};
+  res.render('temp', { json_data: parsed_data });
+});
+
+app.use((req, res) => {
+  res.status(400).json({ error: 'Invalid request' });
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
