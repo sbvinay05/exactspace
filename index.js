17c7db0ecd4918f0b783451ab2edea5e71fd7a28
sbvinay05 <133862038+sbvinay05@users.noreply.github.com>
2023-07-19 03:08:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const bodyParser = require('body-parser');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
